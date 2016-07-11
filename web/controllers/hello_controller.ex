adc1a83b77801a2f7b6fdec68c7c053ed682cddd
AlexKVal <alexkval@gmail.com>
2016-07-12 00:37:50 +0300

    add hello/:messenger route

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -4,4 +4,8 @@ defmodule Tut.HelloController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def show(conn, %{"messenger" => value}) do
+    render conn, "show.html", messenger: value
+  end
 end
