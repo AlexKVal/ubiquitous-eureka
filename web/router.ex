a63d4ce8ff1740e61522ca5e0b219768f8219f75
AlexKVal <alexkval@gmail.com>
2016-07-12 20:15:45 +0300

    resources

--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,24 @@ defmodule Tut.Router do
     get "/", PageController, :index
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+
+    resources "/users", UserController
+    #
+    # it unfolds to the set of routes:
+    #
+    # user_path  GET     /users             Tut.UserController :index
+    # user_path  GET     /users/:id         Tut.UserController :show
+    # user_path  GET     /users/:id/edit    Tut.UserController :edit
+    # user_path  GET     /users/new         Tut.UserController :new
+    # user_path  POST    /users             Tut.UserController :create
+    # user_path  PATCH   /users/:id         Tut.UserController :update
+    #            PUT     /users/:id         Tut.UserController :update
+    # user_path  DELETE  /users/:id         Tut.UserController :delete
+
+    resources "/posts", PostController, only: [:index, :show]
+    # =>
+    # post_path  GET  /posts             Tut.PostController :index
+    # post_path  GET  /posts/:id         Tut.PostController :show
   end
 
   # Other scopes may use custom stacks.
