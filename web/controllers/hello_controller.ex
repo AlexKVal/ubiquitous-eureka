72b581fa3c125f8e4d5552d10bf8cf4e3985b55c
AlexKVal <alexkval@gmail.com>
2016-07-16 00:03:44 +0300

    assign variables for a template. three ways.

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -1,11 +1,19 @@
 defmodule Tut.HelloController do
   use Tut.Web, :controller
 
+  plug :assign_defvalue, "some_default_option" when action in [:index]
+
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> assign(:name, "John Doe")
+    |> render("index.html", message: "A message from Page controller")
   end
 
   def show(conn, %{"messenger" => value}) do
     render conn, "show.html", messenger: value
+  end
+
+  defp assign_defvalue(conn, value) do
+    assign(conn, :defvalue, value)
   end
 end
