baf0f3fa675aebd70e8cb05534017fbc2380dc51
AlexKVal <alexkval@gmail.com>
2016-07-15 23:19:21 +0300

    use flash messages

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule Tut.PageController do
   use Tut.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> put_flash(:info, "Welcome to Phoenix, from flash info!")
+    |> put_flash(:error, "Kinda we have an error.")
+    |> render("index.html")
   end
 end
