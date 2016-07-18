7667f294496bccd92f5442c33bac48b446a31800
AlexKVal <alexkval@gmail.com>
2016-07-18 21:10:03 +0300

    usage of a query param in a text template

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,11 +1,11 @@
 defmodule Tut.PageController do
   use Tut.Web, :controller
 
-  def index(conn, _params) do
+  def index(conn, params) do
     conn
     |> put_flash(:info, "Welcome to Phoenix, from flash info!")
     |> put_flash(:error, "Kinda we have an error.")
-    |> render(:index)
+    |> render(:index, message: params["message"])
   end
 
   def show(conn, %{"id" => id}) do
